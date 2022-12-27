16352351ef47136772c243cf74b766579ea6e69c
Michał Staniszewski <stanisz96@gmail.com>
2022-12-27 11:44:33 +0100

    Add main

---
+++ main.py
@@ -0,0 +1,23 @@
+
+
+
+
+
+
+def main(step_number: int):
+    # Testing
+    if step_number == 0:
+        print("Welcome to Twitter data analysis project!")
+
+
+
+
+
+
+
+
+
+
+
+if __name__=='__main__':
+    main(0)
